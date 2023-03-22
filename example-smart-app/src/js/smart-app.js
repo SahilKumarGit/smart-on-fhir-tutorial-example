f5164d299ebfef14d815e3a11abca14b82724169
SahilKumarGit <email.sahil@yahoo.com>
2023-03-22 15:15:14 +0530

    backup

---
+++ example-smart-app/src/js/smart-app.js
@@ -0,0 +1,34 @@
+async function getData() {
+    return new Promise((resolve, reject) => {
+        console.log('shart-app-promise')
+        FHIR.oauth2.ready((smart) => {
+            if (smart.hasOwnProperty('patient')) {
+                var patient = smart.patient;
+                var pt = patient.read();
+
+                smart.patient.api.fetchAll({
+                    type: 'Observation',
+                    query: {
+                        code: {
+                            $or: [
+                                'http://loinc.org|8310-5',
+                                'http://loinc.org|8302-2',
+                            ]
+                        }
+                    }
+                })
+                    .then(response => resolve({ patient, obv: response }))
+                    .catch(error => reject(error.message));
+                // ----------------
+            } else {
+                reject(`smart.hasOwnProperty('patient') return undefiend.`)
+            }
+        }, (error) => {
+            reject(error.message)
+        });
+    })
+
+}
+
+console.log('shart-app')
+getData().then(console.log).catch(console.log)
