e976f55467d4771e2bbbfa0c6063760173e1bc03
SahilKumarGit <email.sahil@yahoo.com>
2023-03-22 13:23:18 +0530

    x

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -50,17 +50,17 @@
     $('#patentsDetails').html(`
       <div class="card">
         <div class="card-header">
-          Patents Details: <b>${patent.name[0].given.join(' ')} ${patent.name[0].family} </b>
+          Patents Details: <b>${patent?.name[0]?.given.join(' ')} ${patent?.name[0]?.family} </b>
           ${patent.active ? '<span class="badge badge-success">Active</span>' : '<span class="badge badge-danger">Inactive</span>'}</div>
         <div class="card-body">
           <blockquote class="blockquote mb-0">
-            <div>Birth Date: <b>${patent.birthDate}</b></div>
-            <div>Gender: <b>${patent.gender}</b></div>
-            <div>Patents ID: <b>${patent.id}</b></div>
+            <div>Birth Date: <b>${patent?.birthDate}</b></div>
+            <div>Gender: <b>${patent?.gender}</b></div>
+            <div>Patents ID: <b>${patent?.id}</b></div>
             <br/>
             <h5>Contact Info:</h5>
-            ${patent.telecom.map(each => `<div> ${each.system}: ${each.value}</div>`)}            
-            <footer class="blockquote-footer">Last Update: <cite title="Source Title">${new Date(patent.meta.lastUpdated).getUTCDate()}</cite></footer>
+            ${patent?.telecom?.map(each => `<div> ${each?.system}: ${each?.value}</div>`)}            
+            <footer class="blockquote-footer">Last Update: <cite title="Source Title">${new Date(patent?.meta?.lastUpdated).getUTCDate()}</cite></footer>
           </blockquote>
         </div>
       </div>      
